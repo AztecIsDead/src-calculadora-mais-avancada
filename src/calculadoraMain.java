eead07418934925fe7f1e659a70a7a3f1c4cbfc4
Caius Vinícius Araújo de Farias <caius.farias@somosicev.com>
2025-09-04 12:10:23 -0300

    processos e switch

--- src/calculadoraMain.java
+++ src/calculadoraMain.java
@@ -1,14 +1,24 @@
 import java.util.Scanner;
 public class calculadoraMain {
     public static void main(String[] args) {
-    int num1;
-    int num2;
-    int numsalvo;
-    int op;
+
+        processos proc = new processos();
     Scanner scanner = new Scanner(System.in);
+        int num1;
+        int num2;
+        int resultado;
+        int numsalvo;
+        int op;
+
+
         System.out.println("Selecione uma operação");
         System.out.println("1- Adição; 2- Subtração; 3- Multiplicação; 4- Divisão; ");
         op = scanner.nextInt();
+        switch (op) {
+            case 1:
 
+
+
+        }//switch
     }//args bs
     }//main
--- src/processos.java
+++ src/processos.java
@@ -1,2 +1,15 @@
 public class processos {
-}
+    public int soma(int num1, int num2){
+   return num1 + num2;
+   }//soma
+public int sub(int num1, int num2){
+        return num1 - num2;
+}//sub
+public int mult(int num1, int num2){
+       return num1 * num2;
+}//mult
+    public int div(int num1, int num2){
+        return num1 / num2;
+
+    }//div
+}//processos

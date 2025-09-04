362f6b3a50c389258815ccf8bf0d9cbc024b508e
Caius Vinícius Araújo de Farias <caius.farias@somosicev.com>
2025-09-04 09:52:47 -0300

    processos

--- src/calculadoraMain.java
+++ src/calculadoraMain.java
@@ -7,6 +7,8 @@ public class calculadoraMain {
     int op;
     Scanner scanner = new Scanner(System.in);
         System.out.println("Selecione uma operação");
+        System.out.println("1- Adição; 2- Subtração; 3- Multiplicação; 4- Divisão; ");
+        op = scanner.nextInt();
 
     }//args bs
     }//main

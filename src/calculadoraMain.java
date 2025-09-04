d1a833c65f4b078d1687975a4100e967ff7a47ec
Caius Vinícius Araújo de Farias <caius.farias@somosicev.com>
2025-09-04 09:25:36 -0300

    Scanner added

--- src/calculadoraMain.java
+++ src/calculadoraMain.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
 public class calculadoraMain {
     public static void main(String[] args) {
     int num1;
     int num2;
     int numsalvo;
     int op;
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Selecione uma operação");
 
-    
     }//args bs
     }//main

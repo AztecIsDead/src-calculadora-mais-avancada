739a9dfbd2034d66e2bb84da9b83d8d490dbf660
Caius Vinícius Araújo de Farias <caius.farias@somosicev.com>
2025-09-04 23:14:26 -0300

    Socorro
    mais uq CARALHOS ta acontecendo

--- src/calculadoraMain.java
+++ src/calculadoraMain.java
@@ -3,17 +3,18 @@ public class calculadoraMain {
     public static void main(String[] args) {
 
         processos proc = new processos();
+        processosnumsalvo proc2 = new processosnumsalvo();
     Scanner scanner = new Scanner(System.in);
-        double num1;
-        double num2;
+        double num1 = 0;
+        double num2 = 0;
         double numsalvo = 0;
         int op;
-
-
+do {
+    do {
         System.out.println("Selecione uma operação");
-        System.out.println("1- Adição; 2- Subtração; 3- Multiplicação; 4- Divisão; ");
+        System.out.println("1- Adição; 2- Subtração; 3- Multiplicação; 4- Divisão; 6 - Sair ");
         op = scanner.nextInt();
-        if (numsalvo == 0){
+        if (numsalvo == 0) {
             System.out.println("Digite o primeiro numero: ");
             num1 = scanner.nextInt();
             System.out.println("Digite o segundo numero: ");
@@ -24,24 +25,51 @@ public class calculadoraMain {
                 case 1:
                     numsalvo = proc.soma(num1, num2);
                     System.out.println(numsalvo);
+                    break;
                 case 2:
                     numsalvo = proc.sub(num1, num2);
                     System.out.println(numsalvo);
+                    break;
                 case 3:
                     numsalvo = proc.mult(num1, num2);
                     System.out.println(numsalvo);
+                    break;
                 case 4:
                     numsalvo = proc.div(num1, num2);
                     System.out.println(numsalvo);
+                    break;
 
             }//switch
         }//if
-        else{
-            System.out.println("Continuando o cálculo com o numero: " + numsalvo + ", selecione a operação: ");
-            System.out.println("1- Adição; 2- Subtração; 3- Multiplicação; 4- Divisão; ");
-            num2 = 0;
+    } while (numsalvo == 0);
 
+    {
+        System.out.println("Continuando o cálculo com o numero: " + numsalvo + ", selecione a operação: ");
+        System.out.println("1- Adição; 2- Subtração; 3- Multiplicação; 4- Divisão; 6 - Sair; ");
 
-        }//else
+        op = scanner.nextInt();
+        switch (op) {
+            case 1:
+                numsalvo = proc2.somanew(num1, num2);
+                System.out.println(numsalvo);
+                break;
+            case 2:
+                numsalvo = proc2.subnew(num1, num2);
+                System.out.println(numsalvo);
+                break;
+            case 3:
+                numsalvo = proc2.multnew(num1, num2);
+                System.out.println(numsalvo);
+                break;
+            case 4:
+                numsalvo = proc2.divnew(num1, num2);
+                System.out.println(numsalvo);
+                break;
+
+        }//else: switch
+    }//else
+
+}//do
+while(op !=6);
     }//args bs
     }//main
--- src/processosnumsalvo.java
+++ src/processosnumsalvo.java
@@ -1,17 +1,28 @@
+import java.util.Scanner;
+
 public class processosnumsalvo {
+    Scanner scanner = new Scanner(System.in);
     public double somanew(double numsalvo, double num2){
+        System.out.println("Digite o outro numero ");
+        num2 = scanner.nextInt();
         numsalvo = numsalvo + num2;
         return numsalvo;
     }//somanew
     public double subnew(double numsalvo, double num2){
+        System.out.println("Digite o outro numero ");
+        num2 = scanner.nextInt();
         numsalvo = numsalvo - num2;
         return numsalvo;
     }//subnew
     public double multnew(double numsalvo, double num2){
-       numsalvo = numsalvo * num2;
+        System.out.println("Digite o outro numero ");
+        num2 = scanner.nextInt();
+        numsalvo = numsalvo * num2;
        return numsalvo;
     }//multnew
     public double divnew(double numsalvo, double num2){
+        System.out.println("Digite o outro numero ");
+        num2 = scanner.nextInt();
         numsalvo = numsalvo / num2;
         return numsalvo;
     }
